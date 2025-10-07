ce75f82b2528f6c4a96ed27f794df10d0ffe87bf
AmanGit <amanrajdss8252@gmail.com>
2025-10-07 15:33:44 +0530

    Added deployment scripts

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -3,5 +3,5 @@
 chown -R ec2-user:ec2-user /home/ec2-user/app/
 
 # Jar file ko execute karne ki permission dein (agar zaroori ho)
-# Path ko theek karein: JAR file ab /home/ec2-user/app mein hai.
+# JAR file ka path theek kar diya gaya hai.
 chmod +x /home/ec2-user/app/devops-demo-0.0.1-SNAPSHOT.jar
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,4 +1,2 @@
-#!/bin/bash
-# Naya server /home/ec2-user/app directory mein start karein.
-# JAR file ka pura naam mention karein!
+#!/bin/bash Naya server /home/ec2-user/app directory mein start karein.
 nohup java -jar /home/ec2-user/app/devops-demo-0.0.1-SNAPSHOT.jar > /home/ec2-user/app/application.log 2>&1 &
